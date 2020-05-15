3e8be4aa80c19788b363bceeec4f05b2bb16f877
sara altaee <sara.korea96@gmail.com>
2020-05-15 13:56:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "is the champion";
+main.remove();
