02cf3c9de51e63a15eea03bd890f66549298b7f9
sara altaee <sara.korea96@gmail.com>
2020-05-15 13:36:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code here!
 document.body.appendChild(element)
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "is the champion";
+const main =document.querySelector('main').remove();
 main.remove();
